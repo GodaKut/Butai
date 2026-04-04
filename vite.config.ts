99a90606960cda961600319f950cfc4e8f626426
Goda Kutkeviciute <goda.kutkeviciute@gmail.com>
2026-04-04 22:03:35 +0300

    Update ___

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
-      '@': path.resolve(__dirname, './src'),
+      // Alias @ to the scr directory
+      '@': path.resolve(__dirname, './scr'),
     },
   },
 
