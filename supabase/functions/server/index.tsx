58060ca448b9f0487ff213409833ad8f6ad351ec
Goda Kutkeviciute <goda.kutkeviciute@gmail.com>
2026-04-05 16:50:29 +0300

    Adding f

--- supabase/functions/server/index.tsx
+++ supabase/functions/server/index.tsx
@@ -194,7 +194,7 @@ app.post("/make-server-e770b7da/scrape", async (c) => {
 
     //console.log('Extracted floor:', floor);
     console.log('Extracted data:', { address, district, yearBuilt, price, floor, imageUrl });
-    console.log("HAS KAMB:", scrapedData.html.includes("kamb."));
+    console.log("HAS KAMB:", html.includes("kamb."));
     const slice_ex =  html.slice(0, 2000));
     
     return c.json({
